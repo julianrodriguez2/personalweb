e0a3023d9a83bcceec8735241e5e304f52ee9401
Julian Rodriguez <jrodriguezlee2@gmail.com>
2024-05-27 21:51:19 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/personalweb/",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "/src/main.jsx",
-    },
-  },
 });

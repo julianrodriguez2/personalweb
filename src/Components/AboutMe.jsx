cf1b44ac4fc4e1cdaf630032af10cd3ad21d2a1f
Julian Rodriguez <jrodriguezlee2@gmail.com>
2024-05-27 22:33:55 -0700

    new page for resume

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -46,6 +46,8 @@ function AboutMe() {
           <div className="flex justify-center w-full mt-4">
             <a
               href={MyResume}
+              target="_blank"
+              rel="noopener noreferrer"
               className="px-6 py-3 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition-colors duration-300"
             >
               View My Resume
